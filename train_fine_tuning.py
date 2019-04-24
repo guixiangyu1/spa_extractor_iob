25aa33e5e5a581efb4c50d177debc34ca5711442
guixiangyu <guixiangyu1@gmail.com>
2019-04-24 22:29:26 +0800

    acc as metric

--- train_fine_tuning.py
+++ train_fine_tuning.py
@@ -10,10 +10,10 @@ def main():
     config.nepochs          = 200
     config.dropout          = 0.5
     config.batch_size       = 20
-    config.lr_method        = "sgd"
-    config.lr               = 0.1
+    config.lr_method        = "adam"
+    config.lr               = 0.001
     config.lr_decay         = 1.0
-    config.clip             = 2.0 # if negative, no clipping
+    config.clip             = -2.0 # if negative, no clipping
     config.nepoch_no_imprv  = 6
 
     config.dir_model = config.dir_output + "model.finetuning.weights/"
