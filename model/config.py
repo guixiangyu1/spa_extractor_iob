e37c9fb84aac16f9c69feb01244a7b1ff64f4d39
guixiangyu <guixiangyu1@gmail.com>
2019-04-24 18:13:53 +0800

    init

--- model/config.py
+++ model/config.py
@@ -93,7 +93,7 @@ class Config():
     batch_size       = 100
     lr_method        = "adam"
     lr               = 0.01
-    lr_decay         = 0.9
+    lr_decay         = 1.0
     clip             = 5 # if negative, no clipping
     nepoch_no_imprv  = 11
     metrics = 'loss'
