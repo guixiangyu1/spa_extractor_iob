56e4128cdb11c3cf0e6dc550aaa176ac8537494c
guixiangyu <guixiangyu1@gmail.com>
2019-04-28 13:55:14 +0800

    init

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,5 @@
+import sys
+
 from model.data_utils import CoNLLDataset
 from model.ner_model import NERModel
 from model.config import Config
@@ -14,9 +16,17 @@ def main():
     model.restore_session(config.dir_model)
 
     # create dataset
-    test  = CoNLLDataset(config.filename_test, config.processing_word,
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'test':
+            test  = CoNLLDataset(config.filename_test, config.processing_word,
                          config.processing_tag, max_length=None)
-
+        elif sys.argv[1] == 'dev':
+            test = CoNLLDataset(config.filename_dev, config.processing_word,
+                                config.processing_tag, max_length=None)
+    else:
+        assert len(sys.argv) == 1
+        test = CoNLLDataset(config.filename_test, config.processing_word,
+                            config.processing_tag, max_length=None)
     # evaluate and interact
     model.evaluate(test)
     # interactive_shell(model)
