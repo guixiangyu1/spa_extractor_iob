384ecda8633c9b620735669fc226e3aefe350b5b
guixiangyu <guixiangyu1@gmail.com>
2019-08-31 21:09:58 +0800

    init

--- model/config.py
+++ model/config.py
@@ -102,5 +102,5 @@ class Config():
     hidden_size_lstm = 300 # lstm on word embeddings
 
     # NOTE: if both chars and crf, only 1.6x slower on GPU
-    use_crf = True # if crf, training is 1.7x slower on CPU
+    use_crf = False # if crf, training is 1.7x slower on CPU
     use_chars = True # if char embedding, training is 3.5x slower on CPU
